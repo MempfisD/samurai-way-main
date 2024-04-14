6a895c4afba9d77cc6483e9fb19a6680a15f2865
Dmitry Naumov <komrad.nda@yandex.ru>
2024-04-14 23:03:16 +0300

    07. Уроки React JS (что такое Компонента)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,35 @@ import React from 'react'
 import './App.css'
 
 const App = () => {
-	return <div className='App'>Hello, samurai! Let's go!</div>
+	return (
+		<div className='App'>
+			<Header />
+			<Technologies />
+		</div>
+	)
+}
+
+const Header = () => {
+	return (
+		<div>
+			<a href='#s'>Home</a>
+			<a href='#s'>News</a>
+			<a href='#s'>Contacts</a>
+		</div>
+	)
+}
+
+const Technologies = () => {
+	return (
+		<div>
+			<ul>
+				<li>Js</li>
+				<li>React</li>
+				<li>Html</li>
+				<li>CSS</li>
+			</ul>
+		</div>
+	)
 }
 
 export default App
