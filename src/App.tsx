97b06d0af6627fe7fa04da521c62134ada9f5b69
Dmitry Naumov <komrad.nda@yandex.ru>
2024-04-15 23:12:53 +0300

    09. Уроки React JS (import export наших компонент)

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
 import React from 'react'
 import './App.css'
+import Header from './Header'
+import Technologies from './Technologies'
 
 const App = () => {
 	return (
-		<div className='App'>
+		<div>
 			<Header />
 			<Technologies />
-		</div>
-	)
-}
-
-const Header = () => {
-	return (
-		<div>
-			<a href='#s'>Home</a>
-			<a href='#s'>News</a>
-			<a href='#s'>Contacts</a>
-		</div>
-	)
-}
-
-const Technologies = () => {
-	return (
-		<div>
-			<ul>
-				<li>Js</li>
-				<li>React</li>
-				<li>Html</li>
-				<li>CSS</li>
-			</ul>
 		</div>
 	)
 }
---
+++ src/Technologies.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Technologies = () => {
+	return (
+		<div>
+			<ul>
+				<li>Js</li>
+				<li>React</li>
+				<li>Html</li>
+				<li>CSS</li>
+			</ul>
+		</div>
+	)
+}
+
+export default Technologies
